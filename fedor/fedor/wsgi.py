cb611562d4ce3bda8f002625934c1d767a9f05d0
mnmyasis <mnmyasis@gmail.com>
2021-04-16 22:20:03 +0300

    edit directory

--- fedor/fedor/wsgi.py
+++ fedor/fedor/wsgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+#/home/mnmyasis/dev/fedor_app/fedor/fedor
+sys.path.append('/home/mnmyasis/dev/fedor_app/fedor')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fedor.settings')
 
 application = get_wsgi_application()
